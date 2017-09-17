fb6a998959edc2ec3957f784c0901852f8a3eb97
rocco <rocco@hackerstribe.com>
2017-09-17 17:25:08 +0200

    using autoloader

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const debug = require('debug')('termux')
 const execa = require('execa')
+const hasTermux = require('has-termux-api')
+const autoloader = require('./lib/autoloader')
 
+var api = Object.assign({
+  hasTermux: hasTermux.sync()
+}, autoloader())
+
+debug('API loaded:', api)
+
+/*
 // TODO
 execa.shell('termux-battery-status').then(function (result) {
-	console.log(result.stdout)
-	console.log(result.stderr)
+  console.log(result.stdout)
+  console.log(result.stderr)
 }).catch(function (error) {
   console.log(error)
 })
+*/
 
-modulex.exports = api
+module.exports = api
